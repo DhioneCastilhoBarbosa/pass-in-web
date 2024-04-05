68f09484515ab2435987cb3a443a3fe4b0f6b618
Dhione Castilho Barbosa <dh056256@intelbras.com.br>
2024-04-05 16:15:23 -0300

    Created component

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -4,9 +4,9 @@ return(
 <div className='flex flex-col gap-4'>
 <div className="flex gap-3 items-center">
     <h1 className="text-2xl font-bold">Participantes</h1>
-    <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3" >
+    <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg flex items-center gap-3" >
       <Search className='size-4 text-emerald-300'/>
-    <input className="bg-transparent flex-1 outline-none" type="text" placeholder="Buscar participantes..." />
+    <input className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" type="text" placeholder="Buscar participantes..." />
     </div>
   </div>
   <div className='border border-white/10 rounded-lg'>
@@ -14,7 +14,7 @@ return(
       <thead>
         <tr className='border-b border-white/10'>
           <th  style={{width:64}} className='py-3 px-4 text-sm font-semibold text-left'>
-            <input type="checkbox" />
+            <input type="checkbox" className='size-4 bg-black/20 rounded border-white/10'/>
           </th>
           <th className='py-3 px-4 text-sm font-semibold text-left'>Código</th>
           <th className='py-3 px-4 text-sm font-semibold text-left'>Participante</th>
@@ -26,9 +26,9 @@ return(
       <tbody>
         {Array.from({length: 8}).map((_,index)=>{
           return(
-                <tr key={index}className='border-b border-white/10'>
+                <tr key={index}className='border-b border-white/10 hover:bg-white/5'>
                   <td className='py-3 px-4 text-sm text-zinc-300'>
-                    <input type="checkbox" />
+                    <input type="checkbox" className='size-4 bg-black/20 rounded border-white/10'/>
                   </td>
                   <td className='py-3 px-4 text-sm text-zinc-300'>12386</td>
                   <td className='py-3 px-4 text-sm text-zinc-300'>
@@ -57,16 +57,16 @@ return(
             <div className=' inline-flex items-center gap-8'>
               <span>Pagina 1 de 23</span>
               <div className='flex gap-1.5'>
-                <button className=' bg-black/20 border border-white/10 rounded-md p-1.5'>
+                <button className=' bg-white/10 border border-white/10 rounded-md p-1.5'>
                           <ChevronsLeft className='size-4'/>
                 </button>
-                <button className=' bg-black/20 border border-white/10 rounded-md p-1.5'>
+                <button className=' bg-white/10 border border-white/10 rounded-md p-1.5'>
                         <ChevronLeft className='size-4'/>
                 </button>
-                <button className=' bg-black/20 border border-white/10 rounded-md p-1.5'>
+                <button className=' bg-white/10 border border-white/10 rounded-md p-1.5'>
                         <ChevronRight className='size-4'/>
                 </button>
-                <button className=' bg-black/20 border border-white/10 rounded-md p-1.5'>
+                <button className=' bg-white/10 border border-white/10 rounded-md p-1.5'>
                         <ChevronsRight className='size-4'/>
                 </button>
               </div>
